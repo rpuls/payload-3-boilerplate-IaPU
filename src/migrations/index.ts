ecf381cc5d0bbd22d54fb8b52beb01bde6bf17ca
Percy <percy@railway.app>
2024-12-07 12:17:18 +0000

    Initial commit

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241125_222020_initial from './20241125_222020_initial';
+
+export const migrations = [
+  {
+    up: migration_20241125_222020_initial.up,
+    down: migration_20241125_222020_initial.down,
+    name: '20241125_222020_initial'
+  },
+];
